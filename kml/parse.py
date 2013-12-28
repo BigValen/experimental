a5f3b82100508ff27af26e9c667749644c15a1cd
John Looney <john.looney@gmail.com>
2013-12-28 16:34:07 +0000

    Minor change to print out the output of findall.

--- kml/parse.py
+++ kml/parse.py
@@ -5,4 +5,8 @@ test_tree = ET.parse('./test.xml')
 planning_root = planning_tree.getroot()
 test_root = test_tree.getroot()
 
+print test_root.findall('country')
+print planning_root.findall('Document')
+print planning_root.findall('Folder')
+print planning_root.findall('name')
 
