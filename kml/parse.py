b19f28738943dc38a8fd9350e497d2b73c3954c1
BigValen <john.looney@gmail.com>
2013-12-28 18:01:04 +0000

    Fixed it up, so it parses properly & deletes records that don't mention Sutton

--- kml/parse.py
+++ kml/parse.py
@@ -1,7 +1,14 @@
 import xml.etree.ElementTree as ET
+import sys
 
 schema = '{http://www.opengis.net/kml/2.2}'
-planning_tree = ET.parse('./Planning_Applications_Archive.kml')
+
+planning_tree = ET.parse(sys.argv[1])
+
+ET.register_namespace('', 'http://www.opengis.net/kml/2.2')
+ET.register_namespace('atom', 'http://www.w3.org/2005/Atom')
+ET.register_namespace('gx', 'http://www.google.com/kml/ext/2.2')
+
 planning_root = planning_tree.getroot()
 
 kml = planning_root[0]
@@ -9,5 +16,19 @@ folder = kml.find(schema + 'Folder')
 places = folder.findall(schema+'Placemark')
 
 for place in places:
-  print place.tag, place.attrib
+  p_data = place.find(schema+'ExtendedData').find(schema+'SchemaData').findall(schema+'SimpleData');
+  found = False
+  for e in p_data:
+    if 'Location' in e.attrib.values():
+      location = e.text
+      if 'Sutton' in location:
+        found = True
+        print 'Sutton is in', e.text
+      else:
+        print 'Sutton is not in', e.text
+  if not found:
+    print 'deleting', location
+    folder.remove(place)
+
+planning_tree.write('new.kml', xml_declaration = True,method = 'xml')
 
